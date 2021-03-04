98916dee98f618e324f4b7859924bf4354479821
erindoman <erin.doman@gmail.com>
2021-03-03 19:22:30 -0600

    define variables

---
+++ app.js
@@ -0,0 +1,10 @@
+var numSquares = 6
+var colors = []
+var selectedColor
+var squares = document.querySelectorAll('.square')
+var colorDisplay = document.querySelector('#color-display')
+var messageDisplay = document.querySelector('#message')
+var h1 = document.querySelector('h1')
+var reset = document.querySelector('#reset')
+var mode = document.querySelectorAll('.mode')
+var easy = document.querySelector('.mode')
